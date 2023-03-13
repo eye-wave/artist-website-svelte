2925d58d47b5ac0d8e01799e9403cb5d133c4627
_eyewave <89079979+eye-wave@users.noreply.github.com>
2023-03-13 16:32:11 +0100

    add sharp for image processing

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,8 @@ import express from "express"
 import type { Request, Response, NextFunction } from "express"
 import { storageRoute, releasesRoute, demosRoute, shitpostRoute } from "./routes"
 
-function logger(req:Request,res:Response,next:NextFunction) {
+function logger(req:Request,_:Response,next:NextFunction) {
   console.log(`~> Received ${req.method} on ${req.url}`)
-  console.log(`~> Sent ${res.statusCode} on ${req.url}`)
   next()
 }
 
