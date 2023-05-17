ff94e9364f73cc36425c4bb313c79f517bc75dac
_eyewave <89079979+eye-wave@users.noreply.github.com>
2023-05-17 17:53:58 +0200

    list more bugs, update tailwind confi

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,38 +1,18 @@
+const colors = require("tailwindcss/colors")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  darkMode: "class",
-  future: {
-    disableColorOpacityUtilitiesByDefault: true,
-    hoverOnlyWhenSupported: true,
-    respectDefaultRingColorOpacity: true,
-    relativeContentPathsByDefault: true,
-  },
   theme: {
+    fontFamily: {
+      mono: ["ui-monospace"],
+      title: ["Lilita One", "cursive"],
+      sans: ["Montserrat", "sans-serif"],
+    },
     extend: {
       colors: {
-        primary: {
-          50: "#effdfd",
-          100: "#d2f7f9",
-          200: "#aaf0f4",
-          300: "#76e4ec",
-          400: "#39d0dd",
-          500: "#14b4c6",
-          600: "#0892a2",
-          700: "#097684",
-          800: "#0b5e6b",
-          900: "#0d4f5a",
-        },
-      },
-      fontFamily: {
-        title: ["Lilita One", "cursive"],
-        sans: ["Montserrat", "sans-serif"],
-      },
-      screens: {
-        xs: "300px",
-        tall: {
-          raw: "(min-height: 125px)",
-        },
+        primary: colors.violet,
+        gray: colors.zinc,
       },
     },
   },
